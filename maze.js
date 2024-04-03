b86143449b991d8570700ba5dcc93f57413bc6cb
Maria Gregorio <airam.greg@gmail.com>
2024-04-02 23:56:49 -0300

    improve maze walls

--- maze.js
+++ maze.js
@@ -1,25 +1,25 @@
 const generateMaze = (sizeX, sizeY) => {
-	const maze = new Array(sizeY).fill(0)
+	const maze = new Array(sizeX).fill(0)
 	for (let i = 0; i < maze.length; i++) {
-		maze[i] = new Array(sizeX).fill(0)
+		maze[i] = new Array(sizeY).fill(0)
 	}
 	const stack = [[0, 0]]
 	let visited = 0
 	const total = sizeX * sizeY
 
 	while (visited < total - 1) {
-		const current = peekStack(stack);
-		maze[current[0]][current[1]] = {N: 0, S: 0, E: 0, W: 0}
+		const current = peekStack(stack)
+		maze[current[0]][current[1]] = { N: 0, S: 0, E: 0, W: 0 }
 		visited++
 		processCurrentCell(current, stack, sizeX, sizeY, maze)
 	}
 
 	return maze
-};
+}
 
 const peekStack = (stack) => {
 	return stack[stack.length - 1]
-};
+}
 
 const processCurrentCell = (current, stack, sizeX, sizeY, maze) => {
 	const allowedDirections = getAllowedMoves(current, sizeX, sizeY, maze)
@@ -31,13 +31,13 @@ const processCurrentCell = (current, stack, sizeX, sizeY, maze) => {
 	} else {
 		calculateNextMove(current, stack, maze, allowedDirections)
 	}
-};
+}
 
 const calculateNextMove = (current, stack, maze, allowedDirections) => {
-	const nextMoveIndex = Math.floor(Math.random() * allowedDirections.length);
-	const nextMove = allowedDirections[nextMoveIndex];
-	maze[current[0]][current[1]][nextMove] = 1;
-	const nextCell = [];
+	const nextMoveIndex = Math.floor(Math.random() * allowedDirections.length)
+	const nextMove = allowedDirections[nextMoveIndex]
+	maze[current[0]][current[1]][nextMove] = 1
+	const nextCell = []
 
 	switch (nextMove) {
 		case "N":
@@ -51,7 +51,7 @@ const calculateNextMove = (current, stack, maze, allowedDirections) => {
 		case "W":
 			nextCell[0] = current[0]
 			nextCell[1] = current[1] - 1
-			break;
+			break
 		case "E":
 			nextCell[0] = current[0]
 			nextCell[1] = current[1] + 1
@@ -63,18 +63,18 @@ const calculateNextMove = (current, stack, maze, allowedDirections) => {
 }
 
 const getAllowedMoves = (cell, sizeX, sizeY, maze) => {
-	const allowed = [];
+	const allowed = []
 	if (cell[0] > 0 && maze[cell[0] - 1][cell[1]] === 0) {
 		allowed.push("N")
 	}
-	if (cell[0] < sizeY - 1 && maze[cell[0] + 1][cell[1]] === 0) {
+	if (cell[0] < sizeX - 1 && maze[cell[0] + 1][cell[1]] === 0) {
 		allowed.push("S")
 	}
 	if (cell[1] > 0 && maze[cell[0]][cell[1] - 1] === 0) {
 		allowed.push("W")
 	}
-	if (cell[1] < sizeX - 1 && maze[cell[0]][cell[1] + 1] === 0) {
+	if (cell[1] < sizeY - 1 && maze[cell[0]][cell[1] + 1] === 0) {
 		allowed.push("E")
 	}
 	return allowed
-};
+}
--- script.js
+++ script.js
@@ -1,63 +1,74 @@
-const maze = generateMaze(9, 9);
+const mazeRows = 9
+const mazeCols = 15
 
-var ctx = document.getElementById("maze").getContext("2d");
+var cellSize = 30
+var wallSize = 4
+var boardColor = "#D0ECE7"
+var wallColor = "#117A65"
 
-var cellSize = 30;
-var wallSize = 3;
-var cellColor = "#D0ECE7";
-var wallColor = "#117A65";
+let currentX = 0
+let currentY = wallSize * 2
 
-let currentX = 0;
-let currentY = 0;
+const maze = generateMaze(mazeRows, mazeCols)
 
-const drawCell = (cell, neighbors, position) => {
-	ctx.fillStyle = cellColor;
-	ctx.fillRect(position[0], position[1], cellSize, cellSize);
+var ctx = document.getElementById("maze").getContext("2d")
 
-	if (!cell.N && (!neighbors.N || !neighbors.N.S)) {
-		ctx.fillStyle = wallColor;
-		ctx.fillRect(position[0], position[1], cellSize, wallSize);
-	}
+const drawBoard = (rows, cols, cellSize) => {
+	ctx.fillStyle = boardColor
+	ctx.fillRect(currentX, currentY - wallSize, cellSize * cols, cellSize * rows)
+	ctx.fillStyle = wallColor
+	ctx.fillRect(currentX, currentY - wallSize * 2, cellSize * cols, wallSize)
+	ctx.fillStyle = wallColor
+	ctx.fillRect(
+		currentX + cellSize * cols,
+		currentY - wallSize * 2,
+		wallSize,
+		cellSize * rows + wallSize * 2
+	)
+}
+
+const drawWalls = (cell, neighbors, position) => {
 	if (!cell.S && (!neighbors.S || !neighbors.S.N)) {
-		ctx.fillStyle = wallColor;
-		ctx.fillRect(position[0], position[1] + cellSize - wallSize, cellSize, wallSize);
-	}
-	if (!cell.E && (!neighbors.E || !neighbors.E.W)) {
-		ctx.fillStyle = wallColor;
-		ctx.fillRect(position[0] + cellSize - wallSize, position[1], wallSize, cellSize);
+		ctx.fillStyle = wallColor
+		ctx.fillRect(position[0], position[1] + cellSize - wallSize, cellSize, wallSize)
 	}
 	if (!cell.W && (!neighbors.W || !neighbors.W.E)) {
-		ctx.fillStyle = wallColor;
-		ctx.fillRect(position[0], position[1], wallSize, cellSize);
+		ctx.fillStyle = wallColor
+		ctx.fillRect(position[0], position[1] - wallSize, wallSize, cellSize + wallSize)
 	}
-};
+}
 
 const cellNeighbors = (rowI, colI) => {
-	const neighbors = {};
+	const neighbors = {}
 	if (rowI > 0) {
-		neighbors.N = maze[rowI - 1][colI];
+		neighbors.N = maze[rowI - 1][colI]
 	}
 	if (rowI < maze.length - 1) {
-		neighbors.S = maze[rowI + 1][colI];
+		neighbors.S = maze[rowI + 1][colI]
 	}
 	if (colI < maze[rowI].length - 1) {
-		neighbors.E = maze[rowI][colI + 1];
+		neighbors.E = maze[rowI][colI + 1]
 	}
 	if (colI > 0) {
-		neighbors.W = maze[rowI][colI - 1];
+		neighbors.W = maze[rowI][colI - 1]
 	}
-	return neighbors;
-};
-
-for (let i = 0; i < maze.length; i++) {
-	for (let j = 0; j < maze[i].length; j++) {
-		const neighbors = cellNeighbors(i, j);
-		drawCell(maze[i][j], neighbors, [currentX, currentY]);
-		if (j === maze[i].length - 1) {
-			currentX = 0;
-		} else {
-			currentX += cellSize;
-		}
-	}
-	currentY += cellSize;
+	return neighbors
 }
+
+const drawMazeWalls = () => {
+	for (let i = 0; i < maze.length; i++) {
+		for (let j = 0; j < maze[i].length; j++) {
+			const neighbors = cellNeighbors(i, j)
+			drawWalls(maze[i][j], neighbors, [currentX, currentY])
+			if (j === maze[i].length - 1) {
+				currentX = 0
+			} else {
+				currentX += cellSize
+			}
+		}
+		currentY += cellSize
+	}
+}
+
+drawBoard(mazeRows, mazeCols, cellSize)
+drawMazeWalls()
